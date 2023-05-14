62fbd03d9f6b88ad1d461a5c3f5679da4bd2c920
mari <rosascript@gmail.com>
2023-05-14 14:20:22 -0400

    tailwind & postcss init
    + -> add tailwindcss, which also uses postcss, which means i added both.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev" class="font-mono text-blue-400 font-semibold underline"
+		>kit.svelte.dev</a
+	> to read the documentation
+</p>
